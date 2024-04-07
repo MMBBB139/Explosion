436538d7eb0d7b81628dc05e84d39f0e286c3a69
bookworm6 <bilborulesmordor@gmail.com>
2024-04-07 15:24:50 -0700

    added sounds

--- AudioStreamPlayer3D.gd
+++ AudioStreamPlayer3D.gd
@@ -1,15 +1,13 @@
-extends Node3D
+extends AudioStreamPlayer3D
 
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	Input.mouse_mode = Input.MOUSE_MODE_CAPTURED
+	pass # Replace with function body.
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	pass
+	
 
-
-func _on_audio_stream_player_3d_finished():
-	$AudioStreamPlayer3D.play();
