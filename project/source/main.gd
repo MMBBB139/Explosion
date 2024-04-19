a806cef97a2cb841e1300b622a4bf3f3690072e5
Sssmartie <jasonluo66666@Gmail.com>
2024-04-19 08:42:42 +0800

    bomb placement done

--- project/source/main.gd
+++ project/source/main.gd
@@ -1,10 +1,44 @@
 extends Node3D
 
+const RAY_LENGTH = 69
+@export var bomb_scene: PackedScene
+var bomb
+var bombs = []
+var bombsplaced = 0
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
+signal bomb_placed
+
+func _ready():
 	pass
 
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _physics_process(delta):
+	if !bomb:
+		bomb = bomb_scene.instantiate()
+		add_child(bomb)
+		bombs.append(bomb)
+		print("instantiated")
+	if Input.is_action_pressed("placebomb"):
+		print(get_viewport().get_mouse_position())
+		var space_state = get_world_3d().direct_space_state
+		var mousepos = get_viewport().get_mouse_position()
+		var camera = $"CharacterBody3D/Camera/Camera3D"
+		var from = camera.project_ray_origin(mousepos)
+		var to = from + camera.project_ray_normal(mousepos) * RAY_LENGTH
+		
+		var query = PhysicsRayQueryParameters3D.create(from, to)
+		var result = space_state.intersect_ray(query)
+		if "position" in result:
+			print(result["position"])
+			bomb.set_position(result["position"]+Vector3(0,5,0))
+			print(bomb.global_position)
+		if Input.is_action_pressed("actuallyplacebomb"):
+			bomb.fix()
+			bomb = null
+			print(!bomb)
+			bombsplaced += 1
+			$"BombsPlacedIndicator/Label".text = str("Bombs Placed: ") + str(bombsplaced)
+	
 
 func _on_audio_stream_player_3d_finished():
 	$AudioStreamPlayer3D.play();
