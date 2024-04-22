e0462db8ce8573bda1cec6f192654041ea258a58
bookworm6 <bilborulesmordor@gmail.com>
2024-04-22 09:28:55 -0700

    instruction scene fixed

--- project/source/instructions.gd
+++ project/source/instructions.gd
@@ -4,6 +4,7 @@ extends Control
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	$AnimationPlayer.play("fadeIn")
+	
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
@@ -11,16 +12,12 @@ func _process(delta):
 	pass
 
 
+
+
 func _on_button_pressed():
-	print("button pressed")
 	$Fader.show()
 	$AudioStreamPlayer.play()
 	$AnimationPlayer.play("fadeOut")
-	print("playingAnimation")
 
-	
- 
 func changeScene():
-	get_tree().change_scene_to_file("res://source/instructions.tscn")
-	
-
+	get_tree().change_scene_to_file("res://source/main.tscn")
